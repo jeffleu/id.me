18d47ed2ae720a1b06420f10411c482c32b68770
Jeff Leu <jeffreycleu@gmail.com>
2023-12-13 10:20:38 -0800

    Styled columns

--- src/PurchaseRow.tsx
+++ src/PurchaseRow.tsx
@@ -1,5 +1,5 @@
 import { PurchaseType } from './types';
-import { formatDate, formatPrice } from './utils';
+import { formatDate, formatDescription, formatPrice } from './utils';
 
 type PropsType = {
   purchase: PurchaseType;
@@ -10,12 +10,23 @@ const PurchaseRow = ({purchase}: PropsType) => {
 
   return (
     <div className="purchase-row">
-      <div className="purchase-column">{name}</div>
-      <div className="purchase-column"><img className="location-image" src={location}/></div>
+      <div className="purchase-column">
+        <span className="name">{name}</span>
+      </div>
+
+      <div className="purchase-column">
+        <img className="location-image" src={location}/>
+      </div>
+
       <div className="purchase-column">{formatDate(purchaseDate)}</div>
+
       <div className="purchase-column">{category}</div>
-      <div className="purchase-column">{description}</div>
-      <div className="purchase-column">{formatPrice(price)}</div>
+
+      <div className="purchase-column">
+        <span className="description">{formatDescription(description)}</span>
+      </div>
+
+      <div className="purchase-column price">{formatPrice(price)}</div>
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,13 @@ export const formatDate = (inputDate) => {
   return new Intl.DateTimeFormat('en-US', options).format(date);
 };
 
+// Format description
+export const formatDescription = (description) => {
+  const parser = new DOMParser();
+  const decodedText = parser.parseFromString(`<!doctype html><body>${description}`, 'text/html').body.textContent;
+  return decodedText;
+};
+
 // Formats price to include $ and commas
 export const formatPrice = (price) => {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
